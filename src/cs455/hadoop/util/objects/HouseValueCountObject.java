c03af4ffafe2ecc06ed3a2735a77af096ffb9b9e
Chen Wang <gongfunaicha@gmail.com>
2017-04-10 15:49:31 -0600

    Implemented house value count object.

---
+++ src/cs455/hadoop/util/objects/HouseValueCountObject.java
@@ -0,0 +1,57 @@
+package cs455.hadoop.util.objects;
+
+import cs455.hadoop.util.writable.Run1CombinedWritable;
+
+import java.util.ArrayList;
+
+// Used to count the number of houses wihtin each value
+public class HouseValueCountObject {
+    // Used to store count of houses within each value, 20 values intervals in total
+    private long[] valueArray = new long[20];
+
+    public HouseValueCountObject()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            valueArray[i] = 0;
+        }
+    }
+
+    // Perform deep copy in case of modification of input, input array must be in size 20, else perform empty constructor
+    public HouseValueCountObject(long[] valueArray)
+    {
+        if (valueArray.length == 20)
+        {
+            // Deep copy, can be replaced by System.arraycopy?
+            for (int i = 0; i < 20; i++)
+            {
+                this.valueArray[i] = valueArray[i];
+            }
+        }
+        else
+        {
+            // Empty constructor
+            for (int i = 0; i < 20; i++)
+            {
+                valueArray[i] = 0;
+            }
+        }
+    }
+
+    public static HouseValueCountObject aggregate(ArrayList<Run1CombinedWritable> collection)
+    {
+        // TODO: Implement aggregate function
+        return null;
+    }
+
+    // Getter return deep copy of array for safety
+    public long[] getValueArray()
+    {
+        long[] deepCopy = new long[20];
+        for (int i = 0; i < 20; i++)
+        {
+            deepCopy[i] = valueArray[i];
+        }
+        return deepCopy;
+    }
+}
